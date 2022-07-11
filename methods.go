84de208a31b0295a34dddeec73c2855cc665bb59
Andrey Elagin <daddyingrave@gmail.com>
2022-07-11 22:28:05 +0400

    fmt

--- methods.go
+++ methods.go
@@ -25,14 +25,3 @@ func (r *rect) area() int {
 func (r rect) perim() int {
 	return 2*r.width + 2*r.height
 }
-
-func piu() {
-	r := rect{width: 10, height: 5}
-
-	fmt.Println("area: ", r.area())
-	fmt.Println("perim:", r.perim())
-
-	rp := &r
-	fmt.Println("area: ", rp.area())
-	fmt.Println("perim:", rp.perim())
-}
